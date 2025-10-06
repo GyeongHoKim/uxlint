cccf33b75701bbb4fc18f8a8f446ffebc5ede723
GyeongHo Kim <rlarudgh2017@gmail.com>
2025-10-06 21:34:36 +0900

    feat: header component
    - add header component that renders log, title, sub title

--- source/components/index.ts
+++ source/components/index.ts
@@ -3,12 +3,4 @@
  * Centralized exports for all UI components
  */
 
-// Components will be exported here as they are implemented
-// Example:
-// export { Header } from './header.js';
-// export { UserInput } from './user-input.js';
-// export { GradientText } from './gradient-text.js';
-
-// Placeholder export to avoid empty file warning
-export const componentsPlaceholder =
-	'Components will be implemented in subsequent tasks';
+export {Header} from './header.js';
