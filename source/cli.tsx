f91e4bfbd72ffc52aaafb4fe5fb1c332e2836477
GyeongHo Kim <rlarudgh2017@gmail.com>
2025-10-07 16:07:49 +0900

    refactor: 범위를 벗어나는 기능 분리
    - use-keyboard-input은 오직 keyboard 인풋만을 담당하도록 수정
    - user-input이 잘못된 variants type을 사용하고 있는 것을 수정

--- source/cli.tsx
+++ source/cli.tsx
@@ -1,28 +1,6 @@
 #!/usr/bin/env node
 import {render} from 'ink';
-import meow from 'meow';
 import App from './app.js';
 
-const cli = meow(
-	`
-	Usage
-	  $ uxlint
-
-	Options
-		--name  Your name
-
-	Examples
-	  $ uxlint --name=Jane
-	  Hello, Jane
-`,
-	{
-		importMeta: import.meta,
-		flags: {
-			name: {
-				type: 'string',
-			},
-		},
-	},
-);
-
-render(<App name={cli.flags.name} />);
+// @todo meow로 옵션받아야 함
+render(<App />);
